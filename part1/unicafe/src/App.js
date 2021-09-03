08ad00575d3957a866d74c05803fc3b8318ed828
trevor <me@trevorbehnke.com>
2021-09-02 23:12:53 -0700

    completed unicafe 1.6 - 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text={"good"} />
+      <Button handleClick={handleNeutralClick} text={"neutral"} />
+      <Button handleClick={handleBadClick} text={"bad"} />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
-}
+};
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const StatisticsLine = ({ text, stats }) => {
+  if (text === "positive") {
+    return (
+      <tr>
+        <td>{text}: </td>
+        <td>{stats}%</td>
+      </tr>
+    );
+  } else {
+    return (
+      <tr>
+        <td>{text}: </td>
+        <td>{stats}</td>
+      </tr>
+    );
+  }
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  if (good + neutral + bad === 0) {
+    return <p>no feedback yet</p>;
+  } else {
+    return (
+      <div>
+        <table>
+          <tbody>
+            <StatisticsLine text="good" stats={good} />
+            <StatisticsLine text="neutral" stats={neutral} />
+            <StatisticsLine text="bad" stats={bad} />
+            <StatisticsLine text="all" stats={good + neutral + bad} />
+            <StatisticsLine
+              text="average"
+              stats={(good * 1 + bad * -1) / (good + neutral + bad)}
+            />
+            <StatisticsLine
+              text="positive"
+              stats={good * (100 / (good + neutral + bad))}
+            />
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+};
 
 export default App;
