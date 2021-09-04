74a0dd28ddf3a96dbba2ae773784528e8a5162ee
trevor <me@trevorbehnke.com>
2021-09-03 22:48:17 -0700

    completed 2b notes & created phonebook app

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,70 +1,47 @@
-// import React from "react";
-// import Note from "./components/Note";
+import React, { useState } from "react";
+import Note from "./components/Note";
 
-// const App = ({ notes }) => {
-//   return (
-//     <div>
-//       <h1>Notes</h1>
-//       <ul>
-//         {notes.map((note) => (
-//           <Note key={note.id} note={note} />
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
 
-// export default App;
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
 
-// // // ####################################
-// // // MPJME HOF DEMO START
-// // // ####################################
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
 
-// // //
-// // // filter & map demo
-// // //
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
-// // let animals = [
-// //   { name: "jimmy", species: "cat" },
-// //   { name: "tim", species: "dog" },
-// //   { name: "fred", species: "rabbit" },
-// //   { name: "jen", species: "rabbit" },
-// //   { name: "tammy", species: "dog" },
-// //   { name: "bill", species: "rabbit" },
-// //   { name: "snuggles", species: "cat" },
-// //   { name: "poofy", species: "rabbit" },
-// // ];
+  return (
+    <div>
+      <h1>Notes</h1>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? "important" : "all"}
+      </button>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
 
-// // // filter
-// // let cats = animals.filter((i) => i.species === "cat");
-// // console.log("filter only cats", cats);
-
-// // // map
-// // let names = animals.map((i) => {
-// //   return `${i.name} is a ${i.species}`;
-// // });
-// // console.log("animal names", names);
-
-// // //
-// // // reduce demo
-// // //
-
-// // let orders = [
-// //   { amount: 234 },
-// //   { amount: 62 },
-// //   { amount: 684 },
-// //   { amount: 34 },
-// //   { amount: 45 },
-// //   { amount: 574 },
-// // ];
-
-// // // reduce
-// // let total = orders.reduce((sum, order) => {
-// //   console.log(sum, order);
-// //   return sum + order.amount;
-// // }, 0);
-// // console.log("total", total);
-
-// // // ####################################
-// // // MPJME HOF DEMO END
-// // // ####################################
+export default App;
