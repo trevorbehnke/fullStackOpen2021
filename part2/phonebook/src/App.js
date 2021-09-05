d46c80ff39f2ebc0d4865d304775e2c02774f769
trevor <me@trevorbehnke.com>
2021-09-05 14:57:41 -0700

    completed notes 2d &  phonebook 2.15 - 2.17

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+
+import personService from "./services/persons";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -11,8 +12,8 @@ const App = () => {
   const [filtered, setFiltered] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
+    personService.getAll().then((initialPersons) => {
+      setPersons(initialPersons);
     });
   }, []);
 
@@ -22,18 +23,24 @@ const App = () => {
       name: newName,
       number: newNumber,
     };
-    if (
-      persons
-        .map((person) => {
-          return person.name.includes(newName);
-        })
-        .includes(true)
-    ) {
+    if (persons.find((person) => person.name === newName)) {
       alert(`${newName} is already added`);
     } else {
-      setPersons(persons.concat(personObject));
-      setNewName("");
-      setNewNumber("");
+      personService.create(personObject).then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson));
+        setNewName("");
+        setNewNumber("");
+      });
+    }
+  };
+
+  const deletePerson = (id) => {
+    const result = window.confirm(`Delete ${id}?`);
+    if (result === true) {
+      personService.remove(id);
+      personService.getAll().then((newPersons) => {
+        setPersons(newPersons);
+      });
     }
   };
 
@@ -66,7 +73,7 @@ const App = () => {
         onNumberChange={handleNumberChange}
       />
       <h3>Numbers</h3>
-      <Persons personsToShow={personsToShow} />
+      <Persons personsToShow={personsToShow} deletePerson={deletePerson} />
     </div>
   );
 };
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Persons = ({ personsToShow }) => {
+const Persons = ({ personsToShow, deletePerson }) => {
   return (
     <div>
       {personsToShow.map((person) => {
         return (
-          <div key={person.name}>
+          <div key={person.id}>
             {person.name} {person.number}
+            <button onClick={() => deletePerson(person.id)}>delete</button>
           </div>
         );
       })}
