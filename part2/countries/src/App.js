f41e08246ca7b38c95408a1a0fbefa3176159227
trevor <me@trevorbehnke.com>
2021-09-06 18:11:33 -0700

    updated countries and phonebook apps to part2 solution

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,44 +1,44 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
 
-const App = () => {
-  const [countries, setCountries] = useState([]);
-  const [filtered, setFiltered] = useState("");
+// const App = () => {
+//   const [countries, setCountries] = useState([]);
+//   const [filtered, setFiltered] = useState("");
 
-  useEffect(() => {
-    axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
-      setCountries(response.data);
-    });
-  }, []);
+//   useEffect(() => {
+//     axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+//       setCountries(response.data);
+//     });
+//   }, []);
 
-  const handleChange = (event) => {
-    setFiltered(event.target.value);
-  };
+//   const handleChange = (event) => {
+//     setFiltered(event.target.value);
+//   };
 
-  const countriesToShow = countries.filter((country) => {
-    return country.name.toLowerCase().includes(filtered.toLowerCase());
-  });
+//   const countriesToShow = countries.filter((country) => {
+//     return country.name.toLowerCase().includes(filtered.toLowerCase());
+//   });
 
-  return (
-    <div>
-      find countries
-      <input value={filtered} onChange={handleChange} />
-      {(() => {
-        if (filtered === "") {
-          return <div></div>;
-        } else if (countriesToShow.length > 10) {
-          return <div>Too many matches, specify another filter</div>;
-        } else if (countriesToShow.length > 1 && countriesToShow.length <= 10) {
-          return countriesToShow.map((country) => {
-            return <div key={country.name}>{country.name}</div>;
-          });
-        } else if (countriesToShow.length === 1) {
-          // Pick Up Here!!!
-          return <div>Only 1 remaining!</div>;
-        }
-      })()}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       find countries
+//       <input value={filtered} onChange={handleChange} />
+//       {(() => {
+//         if (filtered === "") {
+//           return <div></div>;
+//         } else if (countriesToShow.length > 10) {
+//           return <div>Too many matches, specify another filter</div>;
+//         } else if (countriesToShow.length > 1 && countriesToShow.length <= 10) {
+//           return countriesToShow.map((country) => {
+//             return <div key={country.name}>{country.name}</div>;
+//           });
+//         } else if (countriesToShow.length === 1) {
+//           // Pick Up Here!!!
+//           return <div>Only 1 remaining!</div>;
+//         }
+//       })()}
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;
